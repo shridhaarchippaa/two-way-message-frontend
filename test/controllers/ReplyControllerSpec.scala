f1b8c40e104a2f1ec36a5b04b74583f0e6af79b7
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-03-01 10:17:40 +0000

    wip

--- test/controllers/ReplyControllerSpec.scala
+++ test/controllers/ReplyControllerSpec.scala
@@ -94,7 +94,6 @@ class ReplyControllerSpec extends ControllerSpecBase with MockAuthConnector {
   }
 
   //Please see integration tests for auth failure scenarios as these are handled by the ErrorHandler class
-
   "calling onSubmit()" should {
     val queueId = "p800"
     val messageId = "543e92e101000001006300c9"
@@ -102,7 +101,6 @@ class ReplyControllerSpec extends ControllerSpecBase with MockAuthConnector {
     val requestWithFormData: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequestWithForm.withFormUrlEncodedBody(
       "content" -> "test content"
     )
-
     val replyDetails = ReplyDetails(
       "test content"
     )
@@ -128,111 +126,44 @@ class ReplyControllerSpec extends ControllerSpecBase with MockAuthConnector {
       )
       val result = await(call(controller.onSubmit(queueId, messageId), requestWithFormData))
       result.header.status shouldBe Status.SEE_OTHER
-      result.header.headers("Location") shouldBe "/two-way-message-frontend/message/submitted?maybeId=5c18eb166f0000110204b160"
+      result.header.headers("Location") shouldBe "/two-way-message-frontend/message/customer/p800/543e92e101000001006300c9/submitted?maybeId=5c18eb166f0000110204b160"
     }
 
-  //   "return 303 (SEE_OTHER) when presented with a valid Nino (HMRC-NI) credentials but with an invalid payload" in {
-  //     val bad2wmPostMessageResponse = Json.parse("{}")
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-  //     when(mockTwoWayMessageConnector.postMessage(ArgumentMatchers.eq(replyDetails))(any[HeaderCarrier])).thenReturn(
-  //       Future.successful(
-  //         HttpResponse(Http.Status.CREATED, Some(bad2wmPostMessageResponse))
-  //       )
-  //     )
-  //     val result = await(call(controller.onSubmit(), requestWithFormData))
-  //     result.header.status shouldBe Status.SEE_OTHER
-  //     result.header.headers("Location") shouldBe "/two-way-message-frontend/message/submitted?maybeError=Missing+reference"
-  //   }
+    "return 303 (SEE_OTHER) when presented with a valid Nino (HMRC-NI) credentials but with an invalid payload" in {
+      val bad2wmPostMessageResponse = Json.parse("{}")
+      val nino = Nino("AB123456C")
+      mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
+      when(mockTwoWayMessageConnector.postReplyMessage(ArgumentMatchers.eq(replyDetails), ArgumentMatchers.eq(queueId), ArgumentMatchers.eq(messageId))(any[HeaderCarrier])).thenReturn(
+        Future.successful(
+          HttpResponse(Http.Status.CREATED, Some(bad2wmPostMessageResponse))
+        )
+      )
+      val result = await(call(controller.onSubmit(queueId, messageId), requestWithFormData))
+      result.header.status shouldBe Status.SEE_OTHER
+      result.header.headers("Location") shouldBe "/two-way-message-frontend/message/customer/p800/543e92e101000001006300c9/submitted?maybeError=Missing+reference"
+    }
 
-  //   "return 400 (BAD_REQUEST) when presented with invalid form data" in {
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-  //     val result = call(controller.onSubmit(), badRequestWithFormData)
-  //     status(result) shouldBe Status.BAD_REQUEST
-  //   }
+    "return 400 (BAD_REQUEST) when presented with invalid form data" in {
+      val nino = Nino("AB123456C")
+      mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
+      val result = call(controller.onSubmit(queueId, messageId), badRequestWithFormData)
+      status(result) shouldBe Status.BAD_REQUEST
+    }
 
-  //   "return 303 (SEE_OTHER) when two-way-message service returns a different status than 201 (CREATED)" in {
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-  //     when(mockTwoWayMessageConnector.postMessage(ArgumentMatchers.eq(replyDetails))(any[HeaderCarrier])).thenReturn(
-  //       Future.successful(
-  //         HttpResponse(Http.Status.CONFLICT)
-  //       )
-  //     )
+    "return 303 (SEE_OTHER) when two-way-message service returns a different status than 201 (CREATED)" in {
+      val nino = Nino("AB123456C")
+      mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
 
-  //     val result = await(call(controller.onSubmit(), requestWithFormData))
-  //     result.header.status shouldBe Status.SEE_OTHER
-  //     result.header.headers("Location") shouldBe "/two-way-message-frontend/message/submitted?maybeError=Error+sending+reply+details"
-  //   }
+      when(mockTwoWayMessageConnector.postReplyMessage(ArgumentMatchers.eq(replyDetails), ArgumentMatchers.eq(queueId), ArgumentMatchers.eq(messageId))(any[HeaderCarrier])).thenReturn(
+        Future.successful(
+          HttpResponse(Http.Status.CONFLICT)
+        )
+      )
+
+      val result = await(call(controller.onSubmit(queueId, messageId), requestWithFormData))
+      result.header.status shouldBe Status.SEE_OTHER
+      result.header.headers("Location") shouldBe "/two-way-message-frontend/message/customer/p800/543e92e101000001006300c9/submitted?maybeError=Error+sending+reply+details"
+    }
   }
 
-  // "validation should" should {
-  //   val fakeRequestWithForm = FakeRequest(routes.ReplyController.onSubmit())
-
-  //   "Successfull" in {
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-
-  //     val matchingEmails: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequestWithForm.withFormUrlEncodedBody(
-  //       "queue" -> "queue1",
-  //       "subject" -> "test subject",
-  //       "content" -> "test content",
-  //       "email" -> "test@test.com",
-  //       "confirmEmail" -> "test@test.com"
-  //     )
-  //     val replyDetails = ReplyDetails(
-  //       "queue1",
-  //       "test subject",
-  //       "test content",
-  //       "test@test.com",
-  //       "test@test.com"
-  //     )
-
-  //     when(mockTwoWayMessageConnector.postMessage(ArgumentMatchers.eq(replyDetails))(any[HeaderCarrier])).thenReturn(
-  //       Future.successful(
-  //         HttpResponse(Http.Status.CONFLICT)
-  //       )
-  //     )
-
-  //     val result = await(call(controller.onSubmit(), matchingEmails))
-  //     result.header.status shouldBe Status.SEE_OTHER
-  //   }
-
-  //   "Unsuccessful when emails do not match" in {
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-
-  //     val nonMatchingEmails: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequestWithForm.withFormUrlEncodedBody(
-  //       "queue" -> "queue1",
-  //       "subject" -> "test subject",
-  //       "content" -> "test content",
-  //       "email" -> "test@test.com",
-  //       "confirmEmail" -> "test2@test.com"
-  //     )
-
-  //     val result = await(call(controller.onSubmit(), nonMatchingEmails))
-  //     val document = Jsoup.parse(contentAsString(result))
-  //     document.getElementsByClass("error-summary-list").html() shouldBe "<li><a href=\"#email\">Email addresses must match. Check them and try again.</a></li>"
-  //     result.header.status shouldBe Status.BAD_REQUEST
-  //   }
-
-  //   "Unsuccessful when subject is too long" in {
-  //     val nino = Nino("AB123456C")
-  //     mockAuthorise(Enrolment("HMRC-NI"))(Future.successful(Some(nino.value)))
-
-  //     val nonMatchingEmails: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequestWithForm.withFormUrlEncodedBody(
-  //       "queue" -> "queue1",
-  //       "subject" -> "a" * 66,
-  //       "content" -> "test content",
-  //       "email" -> "test@test.com",
-  //       "confirmEmail" -> "test@test.com"
-  //     )
-
-  //     val result = await(call(controller.onSubmit(), nonMatchingEmails))
-  //     result.header.status shouldBe Status.BAD_REQUEST
-  //     val document = Jsoup.parse(contentAsString(result))
-  //     document.getElementsByClass("error-summary-list").html() shouldBe "<li><a href=\"#subject\">Maximum length is 65</a></li>"
-  //   }
-  // }
 }
