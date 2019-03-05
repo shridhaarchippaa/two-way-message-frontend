0416a75fd0937571e8c14dea6117de5311f3bf55
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-03-05 13:55:02 +0000

    [DC-1743] Add customer reply form

---
+++ app/forms/ReplyFormProvider.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.mappings.Mappings
+import javax.inject.Inject
+import models.ReplyDetails
+import play.api.data.Form
+import play.api.data.Forms._
+
+class ReplyFormProvider @Inject() extends FormErrorHelper with Mappings {
+  private val SUBJECT_MAX_LENGTH = 65
+
+  def apply(): Form[ReplyDetails] =
+    Form(
+      mapping(
+        "content" -> text()
+      )(ReplyDetails.apply)(ReplyDetails.unapply)
+    )
+
+}
